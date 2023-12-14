3bf834ae808c25324239150f9db8e17683163c30
KBhautik6 <153491994+KBhautik6@users.noreply.github.com>
2023-12-14 18:32:00 +0530

    Revert "Component"

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       Hello World
-      <p>this is p tag</p>
     </div>
   );
 }
--- src/stories/Topograhy.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {Heading} from "../Typography/Typography";
-
-export default {
-  title: 'Heading',
-  component: Heading,
-};
-
-export const Moves = () => <Heading />;
